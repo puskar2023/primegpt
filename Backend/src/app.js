16fab7285cbcdb9268fec55c5e31c9eb46b493c2
puskar2023 <puskarmandal04@gmail.com>
2026-02-13 00:12:02 +0530

    Refactor route handling for serving static files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -24,8 +24,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 module.exports = app;
